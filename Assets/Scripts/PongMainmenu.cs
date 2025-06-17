12941323a91490c15af08383432637a895d30237
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-06-17 23:39:33 +0500

    Update PongMainmenu.cs

--- Assets/Scripts/PongMainmenu.cs
+++ Assets/Scripts/PongMainmenu.cs
@@ -1,11 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class PongMainmenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI score;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score.text = "0";
     }
 
     // Update is called once per frame
