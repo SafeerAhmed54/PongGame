877243c42b7f3a9b6da677eb2e650f6c241b811d
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-06-24 23:22:19 +0500

    Update PongMainmenu.cs

--- Assets/Scripts/PongMainmenu.cs
+++ Assets/Scripts/PongMainmenu.cs
@@ -5,11 +5,13 @@ public class PongMainmenu : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI score;
     [SerializeField] private TextMeshProUGUI highScore;
+    [SerializeField] private TextMeshProUGUI playerName;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         score.text = "0";
         highScore.text = "129";
+        playerName.text = "Player 1";
     }
 
     // Update is called once per frame
