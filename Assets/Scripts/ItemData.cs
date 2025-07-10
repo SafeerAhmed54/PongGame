058b0cde3ca4e6f89370d43fdef1307f028e9502
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-07-11 01:11:17 +0500

    Updated Scriptable Object

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemData", menuName = "Inventory/Item Data")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public int damage;
+    public Sprite icon;
+}
---
+++ Assets/Scripts/ItemPick.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ItemPick : MonoBehaviour
+{
+    public ItemData data;
+
+    void Start()
+    {
+        Debug.Log("Picked up: " + data.itemName + ", Damage: " + data.damage);
+    }
+}
